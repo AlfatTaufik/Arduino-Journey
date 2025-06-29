5fc731c8c67a9146afee265a0b2a592a314598e6
Alfat <alfat601@smk.belajar.id>
2025-06-30 01:20:11 +0700

    Serial Print

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -2,13 +2,17 @@ int delays = 3000;
 
 void setup() {
   // put your setup code here, to run once:
+  // Serial.begin(9600);
+  // Serial.print("Test");
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.begin(9600);
   analogWrite(LED_BUILTIN, 256);
   delay(delays);
+  Serial.println("Hai, aku sudah menjeda");
   analogWrite(LED_BUILTIN, 0);
   delay(delays);
 }
